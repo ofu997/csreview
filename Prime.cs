1b7b11c5d9decf2a30f576b0e4b2444df4f881d8
ofu997 <ofu997@gmail.com>
2020-03-01 17:32:34 -0800

    control main with switch statements

--- Prime.cs
+++ Prime.cs
@@ -1,32 +1,67 @@
 using System;
 using System.Collections.Generic;
 
-namespace csreview{
+namespace csreview
+{
     class Prime
     {
         // If it is greater than 1 and cannot be written as the product of two smaller natural numbers
-        public static Boolean DetermineIfPrime(int value) 
+        public static Boolean DetermineIfPrime() 
         {
+            Console.WriteLine("\n\nTest if a number is prime (greater than 1, and not the product of two smaller natural numbers).");
+            String answer=Console.ReadLine();
+            int intAnswer=int.Parse(answer);
             List<int> factors = new List<int>();
-            bool greaterThanOne = value > 1? true: false; 
+            bool greaterThanOne = intAnswer > 1? true: false; 
             // note: numbers less than 1 won't run here 
-            for (int i = 1; i < value; i++){
-                if(value % i == 0){
+            for (int i = 1; i < intAnswer; i++){
+                if(intAnswer % i == 0){
                     factors.Add(i);
                 }
             }
+            bool isPrime; 
             Console.WriteLine("greater than 1: {0}", greaterThanOne);
-            Console.WriteLine("number of factors smaller than {0}: {1}", value, factors.Count);
+            Console.WriteLine("number of factors smaller than {0}: {1}", intAnswer, factors.Count);
             factors.ForEach(i => Console.Write("factor: {0}\t", i));
             Console.WriteLine("\nPrime?");
             // if not prime
-            if ( factors.Count > 1 || value <= 1 ){
-                return false;
+            if ( factors.Count > 1 || intAnswer <= 1 ){
+                isPrime=false; 
+                Console.WriteLine("false");
+
+                // return false;
             }
             // if prime
             else {
-                return true;
+                isPrime=true; 
+                Console.WriteLine("true");
+                // return true;
             }
+            return isPrime; 
+
         } // determine if prime
+
+        public static void AskAgain()
+        {
+            bool askLoop = true;
+            while (askLoop)
+            {
+                Console.WriteLine("Test another number?(y/n)");
+                String testAgain = Console.ReadLine();
+                if (testAgain == "y"){
+                    askLoop = true;
+                    // Console.WriteLine("Enter new number:");
+                    // String answer2 = Console.ReadLine();
+                    // Console.WriteLine("You picked {0}", answer2);    
+                    Prime.DetermineIfPrime();
+                }
+                else if(testAgain == "n"){
+                    askLoop=false;
+                }
+                else {
+                    askLoop=false;
+                }
+            }   
+        }
     } // class Prime
 } //namespace
